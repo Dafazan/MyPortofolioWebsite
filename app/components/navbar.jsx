954d07b0d17c90890c575cc3eea22758ca0c5ed1
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-09-05 17:35:53 +0700

    flowbite

---
+++ app/components/navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function NavbarCustom() {
+    return (
+        <>
+
+            <div className='fixed w-full'>
+                <nav class="border-gray-20 bg-gray-900">
+                    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                        <a href="https://dafazan.my.id" class="flex items-center">
+                            <img src="dafazan.png" class="h-8 mr-3" alt="Dafazan Logo" />
+
+                        </a>
+                        <div class="flex items-center">
+                            <a href="tel:5541251234" class="mr-6 text-sm  text-white hover:underline">(62) 858-6471 4906</a>
+                            <a href="#" class="text-sm  text-blue-500 hover:underline">Contact</a>
+                        </div>
+                    </div>
+                </nav>
+                <nav class="bg-gray-700">
+                    <div class="max-w-screen-xl px-4 py-3 mx-auto">
+                        <div class="flex items-center">
+                            <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                                <li>
+                                    <a href="#" class="text-white hover:underline" aria-current="page">Home</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-white hover:underline">Company</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-white hover:underline">Team</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-white hover:underline">Features</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+        </>
+    )
+}
+
+export default NavbarCustom
