e86ce7d1200e20fb25654688094150f4d618b3e2
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-09-08 00:36:52 +0700

    testing a/n web

---
+++ app/components/detailcard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DetailCard() {
+    return (
+        <div className='w-[405px] h-[347px] bg-slate-700 rounded-xl'>detailcard</div>
+    )
+}
+
+export default DetailCard
