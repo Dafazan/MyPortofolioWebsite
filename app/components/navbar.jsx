955d2ed25c1781ced849abec5a176d709a285c19
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-09-16 15:00:37 +0700

    update uhuy

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -4,7 +4,7 @@ function NavbarCustom() {
     return (
         <>
 
-            <div className='fixed w-full z-10'>
+            <div className=' w-full z-10'>
                 <nav class="border-gray-20 bg-gray-900">
                     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                         <a href="https://dafazan.my.id" class="flex items-center">
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ import Splide from '@splidejs/splide'
 export default function Home() {
   return (
     <main >
-      <div className=' w-screen md:h-[100px] h-[70px]'> </div>
-      <div className=' w-screen h-4/5 bg-gray-900 md:flex md:pt-20'>
+
+      <p className='md:text-7xl text-3xl font-bold items-center text-black bg-yellow-300'>Website under development</p>
+      <div className=' w-screen h-4/5 bgl md:flex md:pt-20'>
         <div className=' md:w-2/6 w-[0px] md:pt-0 pt-10'>
           <div className='sm:me-20 md:ms-40 md:m-10 m-10 '>
             <img className='' src="logoM.png" alt="" />
@@ -19,37 +20,60 @@ export default function Home() {
             <p className='text-[#45FFCA] '>Hi, My name is</p>
             <p className='md:text-7xl text-4xl font-bold'>M. Daffa Fauzan</p>
             <br />
-            <p className='text-[#45FFCA] md:text-6xl text-3xl font-semibold hover:text-gray-900 hover:bg-[#45FFCA] pb-3'>Designer.</p>
-            <p className='text-[#45FFCA] md:text-6xl text-3xl font-semibold hover:text-gray-900 hover:bg-[#45FFCA] md:pb-3 pb-10'>Software Engineer.</p>
+            <p className='text-[#45FFCA] md:text-6xl text-3xl font-semibold hover:text-gray-900 hover:bg-[#45FFCA] pb-3 duration-200 ease-in-out'>Designer.</p>
+            <p className='text-[#45FFCA] md:text-6xl text-3xl font-semibold hover:text-gray-900 hover:bg-[#45FFCA] md:pb-3 pb-10 duration-200 ease-in-out'>Software Engineer.</p>
           </div>
         </div>
       </div >
-      <div className=' h-[200px] md:h-full w-screen bg-gray-900'>
+      <div className=' h-[200px] md:h-full w-screen bgl'>
         <div className="grid justify-center md:pt-0 pt-[100px]">
           <a href="#aboutme">
-            <button className='w-[200px] h-[50px] text-[#45FFCA] bg-gray-800 rounded-full hover:text-gray-800 hover:bg-[#45FFCA]'>About Me</button>
+            <button className='w-[140px] h-[50px] hover:w-[220px] duration-75 ease-in-out text-[#45FFCA] bg-gray-800 rounded-full hover:text-gray-800 hover:bg-[#45FFCA] hover:text-2xl'>About Me</button>
           </a>
         </div>
       </div>
-      <div id='aboutme' className="w-screen h-[100px] bg-gray-900"></div>
+      <div id='aboutme' className="w-screen h-[100px] bgl"></div>
 
-      <div className="w-screen h-[200px]">
-
-      </div>
-
-      <div id='porto' className="flex flex-col p-10 md:pt-10 pt-0 sm:ps-20 sm:pe-20 md:ps-40 md:pe-40 bg-gray-900">
-        <p className='text-4xl font-semibold pb-5'>Portofolio</p>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center gap-3'>
-          <CardInfo />
-          <CardInfo />
-          <CardInfo />
-          <CardInfo />
-          <CardInfo />
+      <div className='bgd md:px-20 p-10'>
+        <div className='bg-red-900 md:flex'>
+          <div className='md:w-8/12 bg-green-300'>About me</div>
+          <div className='md:w-4/12 bg-red-800'>image</div>
+        </div>
+        <div className='bg-violet-400 mt-20'>
+          Skills
+        </div>
+        <div className='bg-violet-800 grid grid-cols-2 md:grid-cols-4 gap-5'>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
+          <div className='bgd'>skillGrid</div>
         </div>
       </div>
 
+      <div className='bgl md:px-20 p-10'>
+        <div className='bg-red-900'>Where ive worked</div>
+        <div className='bg-green-300 md:px-48 md:flex'>
+          <div className='bg-violet-800 md:w-3/12'>where</div>
+          <div className='bg-violet-400 md:w-9/12'>desc</div>
+        </div>
+      </div>
 
-
+      <div className='bgd md:px-20 p-10'>
+        <div className='bg-red-900'>Things ive done</div>
+        <div className='bg-violet-900 md:flex'>
+          <div className='bg-violet-600 md:w-7/12'>image</div>
+          <div className='bg-violet-300 md:w-5/12 flex-row'>
+            <div className='bg-red-600'>title</div>
+            <div className='bg-red-800'>desc</div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
