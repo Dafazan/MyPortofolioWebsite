c4bbd8b0442f505fb1fd93db564b13d982f0727b
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-09-19 22:20:18 +0700

    date fix

--- app/components/workplaces/projok.jsx
+++ app/components/workplaces/projok.jsx
@@ -4,7 +4,7 @@ function ProjokDesc() {
     return (
         <>
             <p className='text-xl'>Creator at <span className='txt1'>Programmer Jongkok</span></p>
-            <p className='txt1'>June 2022 - Present</p>
+            <p className='txt1'>June 2023 - Present</p>
             <div className='h-[2px] bg-white mb-2'></div>
             <p>Definetely a description here</p>
         </>
--- app/components/workplaces/saptaloka.jsx
+++ app/components/workplaces/saptaloka.jsx
@@ -4,7 +4,7 @@ function SaptalokaDesc() {
     return (
         <>
             <p className='text-xl'>Developer at <span className='txt1'><a href="https://saptalokadigital.com/">Saptaloka Digital</a></span></p>
-            <p className='txt1'>May 2022 - Present</p>
+            <p className='txt1'>May 2023 - Present</p>
             <div className='h-[2px] bg-white mb-2'></div>
             <p>Also a description here</p>
         </>
